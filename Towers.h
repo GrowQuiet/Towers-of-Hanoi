b3587b7b74cf3f52863ef66f1e66f9ed40179bca
Tom mulvey <tommulvey5567@gmail.com>
2018-04-21 11:43:58 -0400

    private member data added

--- Towers.h
+++ Towers.h
@@ -7,7 +7,12 @@ class Towers {
 	public :
 		//void print() const;
 	private :
-	
+		// 3 rods to move discs onto 
+		stack<int> m_rod1;
+		stack<int> m_rod2;
+		stack<int> m_rod3;
+		// max num of discs should be like 14
+		unsigned discs;
 };
 
 #endif /* TOWERS_H */
