48a1acab316062efe17c15e76902cbe14db375a3
Tom mulvey <tommulvey5567@gmail.com>
2018-04-21 11:33:48 -0400

    skelton of program

---
+++ Towers.cpp
@@ -0,0 +1,5 @@
+#include <iostream>
+#include "Towers.h"
+
+using namespace std;
+
---
+++ Towers.h
@@ -0,0 +1,11 @@
+#ifndef TOWERS_H
+#define TOWERS_H
+
+class Towers {
+	public :
+	
+	private :
+	
+};
+
+#endif /* TOWERS_H */
